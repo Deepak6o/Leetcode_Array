9f12d5cf8d75a85d188451fe6efa4454f5c716b1
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-04-28 18:07:08 +0530

    Create Single Number III .cpp

---
+++ Single Number III .cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int> m;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        for(auto x: m)
+        {
+            if(x.second==1)
+            {
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
